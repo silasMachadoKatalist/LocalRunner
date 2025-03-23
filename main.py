445f3f60eee75a47fdf6753557a60a6f7e98c275
Silas de Assis Machado <silas.machado@katalist.com>
2025-03-23 17:32:13 +0000

    update main start to not handle app start context(avoid context problems)

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-import uvicorn
 import os
+from threading import Thread
+from file_watcher import start_file_watcher
+import uvicorn
+from context_execution_singleton import ContextExecutionSingleton
 
 if __name__ == "__main__":
     project = os.getenv("PROJECT")
     port = int(os.getenv("PORT", 3000))  # Default to port 3000 if PORT is not set
+    workspace_folder = os.getenv("PYTHONPATH", os.getcwd())  # Use PYTHONPATH as the workspace folder
+    print(f"Workspace Folder: {workspace_folder}")
 
+    app_starter = "project_app:app" if project else "dynamic_app:app"
     if project:
-        # Start the app in project mode
         print(f"Starting in Project Mode with PROJECT={project}")
-        uvicorn.run("project_app:app", host="0.0.0.0", port=port, reload=True)
     else:
-        # Start the app in dynamic mode
         print("Starting in Dynamic Mode")
-        uvicorn.run("dynamic_app:app", host="0.0.0.0", port=port, reload=True)
+
+    print("Await for the application to start...")
+    uvicorn.run(
+        app_starter,
+        host="0.0.0.0",
+        port=port,
+        reload=True
+    )
